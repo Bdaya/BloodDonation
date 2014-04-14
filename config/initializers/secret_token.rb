de264e6cb5b081c40e771b7c7c4b5620529ffe30
just3obad <just3obad@gmail.com>
2014-04-14 14:33:59 +0200

    Initial Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BloodDonation::Application.config.secret_token = 'a89ad9503e39c65250d5eae4d94cdf71b174f4b167a1e3d719d8dddd92d1c82218ffd1f3554028e53792902a8d0a65618a256df4099f2f2449ae94a7052a9c34'
