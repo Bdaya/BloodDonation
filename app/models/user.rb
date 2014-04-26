7333f9ee9a236af81ab59227c104bba8e5192638
jihan2032 <jihan2032@gmail.com>
2014-04-26 15:17:25 +0200

    adding users requests page

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,18 @@ class User
   field :ID , type: String
   validates_presence_of :ID, :message=> "Must enter your ID!"
   field :state , type: Boolean , default: true
+ 
+  #  how many times this user REALLY donated through the system
+  field :no_of_donates, type: Integer, default: 0
 
+  #  this is to denote trophies level (say 2 trophies = level 1)
+  field :no_of_trophies, type: Integer, default: 0  
+  #  and this one is to denote the type of the tropy (say level 2 = silver star)
+  field :trophies_level, type: Integer, default: 0  
+  
+  #  is_available is for the user to choose whether to toggle his availibilty button or NOT
+  field :is_available, type: Boolean, default: true
+  #  here,, a user can only donate once in every 3 months (taqreeban)
   ## Confirmable
   # field :confirmation_token,   type: String
   # field :confirmed_at,         type: Time
