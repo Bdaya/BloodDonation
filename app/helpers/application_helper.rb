8b0a1e0322c8e588cbc33601ce324f55c194592c
HebaFareed <heba.f.fawzy@gmail.com>
2014-08-13 13:15:34 +0300

    fix login form issue at main page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
 	  def title(page_title)
   content_for :title, page_title.to_s
   end
+
+	 def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
