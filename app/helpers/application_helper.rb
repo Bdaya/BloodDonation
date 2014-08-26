970cd5ee150c6b7dbc9369eea7c540864fe56892
HebaFareed <heba.f.fawzy@gmail.com>
2014-08-26 15:54:38 +0300

    FrontEnd

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,16 @@ module ApplicationHelper
   def blood_types
     BloodType.all.map{ |b| [b.type, b.id]}
   end
+
+  	 def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
