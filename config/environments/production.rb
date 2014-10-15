e624fcbd416f59a76dae635b46047732288437d6
HebaFareed <heba.f.fawzy@gmail.com>
2014-10-15 01:39:46 -0400

    nonesense

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,8 @@ Faselty::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  config.assets.compile = true
+  config.assets.precompile += %w( app.css )
   # Generate digests for assets URLs.
   config.assets.digest = true
 
